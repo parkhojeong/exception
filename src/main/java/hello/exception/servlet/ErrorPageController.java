bb9ccfeb7f2796cc70c42b568ee86a403fea683e
hojeong <parkhj062@gmail.com>
2026-01-12 20:24:12 +0900

    [96. 서블릿 예외 처리 - 오류 페이지 작동 원리]
    - WAS forward matched error handler

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -3,21 +3,50 @@ package hello.exception.servlet;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Iterator;
 
 @Slf4j
 @Controller
 public class ErrorPageController {
+    //RequestDispatcher 상수로 정의되어 있음
+    public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
+
     @RequestMapping("/error-page/404")
-    public String  error404(HttpServletRequest request, HttpServletRequest response) {
+    public String error404(HttpServletRequest request, HttpServletRequest response) {
         log.info("error404");
+        printErrorInfo(request);
         return "error-page/404";
     }
 
     @RequestMapping("/error-page/500")
-    public String  error500(HttpServletRequest request, HttpServletRequest response) {
+    public String error500(HttpServletRequest request, HttpServletRequest response) {
         log.info("error500");
+        printErrorInfo(request);
         return "error-page/500";
     }
+
+    private void printErrorInfo(HttpServletRequest request) {
+        Iterator<String> iterator = request.getAttributeNames().asIterator();
+        while (iterator.hasNext()) {
+            String attributeName = iterator.next();
+            log.info("{}: {}", attributeName, request.getAttribute(attributeName));
+        }
+        log.info("ERROR_EXCEPTION: {}", request.getAttribute(ERROR_EXCEPTION));
+        log.info("ERROR_EXCEPTION_TYPE: {}", request.getAttribute(ERROR_EXCEPTION_TYPE));
+        log.info("ERROR_MESSAGE: {}", request.getAttribute(ERROR_MESSAGE));
+        log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
+        log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
+        log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        log.info("dispatcherType:{}", request.getDispatcherType());
+    }
 }
+
+
+
